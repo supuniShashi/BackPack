965f838038b8327e837e92aca5a1e233e2493b28
Nu000 <nmmullegama@gmail.com>
2019-07-06 09:22:07 +0530

    guest details form

--- src/app/guest/guest.component.ts
+++ src/app/guest/guest.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./guest.component.css']
 })
 export class GuestComponent implements OnInit {
-
+  firstName = 'aa';
   constructor() { }
 
   ngOnInit() {
